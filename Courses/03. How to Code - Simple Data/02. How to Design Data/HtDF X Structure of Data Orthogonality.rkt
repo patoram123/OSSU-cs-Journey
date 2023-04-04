e5da13c1f195290db873ae97839840f6de60bdad
patoram123 <patramirezv@udd.cl>
2023-04-03 23:31:42 -0400

    Third Commit

---
+++ Courses/03. How to Code - Simple Data/02. How to Design Data/HtDF X Structure of Data Orthogonality.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |HtDF X Structure of Data Orthogonality|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+FORM OF DATA
+                      |               
+PRIMTIVE              |                   NON-PRIMITIVE (DESIGNED W/ HtDD)
+                      |
+String Number...      |    atomic   distinct   interval   enumeration   itemization    compound  list  tree
+                      |
+                      |   CityName    false    SeatNum    LetterGrade    CountDown
+-------------------------------------------------------------------------------------------------------------->
+
+HtDF double, yell, area    best?                aisle?     bump-up        countdown-
+   image-area, tall                                                       to-display
+
+
+
+
+HtDW
